150b51d30fb121c0c882aa9abc40443a59affbe2
Jayke770 <jakevelasco40@gmail.com>
2022-12-01 07:58:03 +0800

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [{
+        name: "algo-staking",
+        script: "./server/app.mjs",
+        env: {
+            NODE_ENV: "development"
+        },
+        env_production: {
+            NODE_ENV: "production",
+            PORT: 80
+        }
+    }]
+}
