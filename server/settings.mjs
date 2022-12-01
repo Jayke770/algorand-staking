691092c04130b8dc52a8472ba0aa11b53bcd4b66
Jayke770 <jakevelasco40@gmail.com>
2022-12-01 08:26:03 +0800

    .

--- server/settings.mjs
+++ server/settings.mjs
@@ -3,7 +3,7 @@ import next from 'next'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
-const port = process.env.NODE_ENV !== 'production' ? 3000 : 80
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const ORIGIN = [process.env.HOST]
 const app = next({ dev })
